0ee0307ca5839dbf6cf3701b7dff5d91558b802b
ProSolvo <timqha@gmail.com>
2015-08-16 21:41:57 +0300

    clear apliccation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,31 +24,7 @@ class ApplicationController < ActionController::Base
     unless current_user.admin?
       render :layout => 'application', :nothing => true
     end
-    #redirect_to("/#/") unless
-    #render json: {}, status: :unauthorized if get_current_user.nil?
   end
-=begin
-
-  def authenticate_current_user
-    render json: {}, status: :unauthorized if get_current_user.nil?
-  end
-
-  def get_current_user
-    return nil unless cookies[:auth_headers]
-    auth_headers = JSON.parse cookies[:auth_headers]
-
-    expiration_datetime = DateTime.strptime(auth_headers["expiry"], "%s")
-    current_user = User.find_by uid: auth_headers["uid"]
-
-    if current_user &&
-        current_user.tokens.has_key?(auth_headers["client"]) &&
-        expiration_datetime > DateTime.now
-
-      @current_user = current_user
-    end
-    @current_user
-  end
-=end
 
   protected
 
